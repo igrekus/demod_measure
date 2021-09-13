f88edd2a81500eaa320b4406063d36f9e661077d
igrekus <upload234@mail.ru>
2021-09-13 18:56:41 +0300

    wip: add result table stub

---
+++ resulttablewidget.py
@@ -0,0 +1,21 @@
+from PyQt5.QtWidgets import QTableView, QWidget, QVBoxLayout
+
+from measuremodel import MeasureModel
+
+
+class ResultTableWidget(QWidget):
+
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+
+        self._model = MeasureModel(parent=self)
+        self._table = QTableView()
+        self._table.setModel(self._model)
+
+        self._layout = QVBoxLayout()
+        self._layout.addWidget(self._table)
+
+        self.setLayout(self._layout)
+
+    def updateResult(self):
+        self._model.update()
